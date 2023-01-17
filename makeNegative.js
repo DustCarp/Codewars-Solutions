8d3c99b516ce4ff2c856eb08c19835c35782f363
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-01-16 19:49:14 -0500

    Create makeNegative.js

---
+++ makeNegative.js
@@ -0,0 +1,12 @@
+//function that returns a given number as a negative number
+
+function makeNegative(num) {
+    if(num>0){
+      return -num
+    }else if(num<0){
+      return num
+    }
+    else{
+      return num
+    }
+  }
