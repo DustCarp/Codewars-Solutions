4b88b2300bc4ebff1445bbffd8fd80e4b67bb136
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-01-21 21:34:42 -0500

    Create sumPositive.js

---
+++ sumPositive.js
@@ -0,0 +1,11 @@
+//Function for returning the sum of all the positive values in an array
+
+function positiveSum(arr) {
+  var total = 0;    
+  for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+    if (arr[i] > 0) {                   // if arr[i] is greater than zero
+      total += arr[i];                  // add arr[i] to total
+    }
+  }
+  return total;                         // return total
+}
