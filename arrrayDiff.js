fa4232f8e58ba083b448e5a5288eabec39c37cd9
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-02-09 21:54:09 -0500

    Create arrrayDiff.js

---
+++ arrrayDiff.js
@@ -0,0 +1,11 @@
+//Function that takes in two arrays and searches for repeats between the arrays. It then returns an array of the numbers that do not repeat. 
+
+function arrayDiff(a, b) {
+for(let i = a.length - 1; i >= 0; i--){
+  for(let j = b.length -1; j>=0; j--){
+        if(a[i] == b[j]){
+        a.splice(i, 1);}
+  }
+}
+  return a
+}
