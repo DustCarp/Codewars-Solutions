d17bb936093f0e531c22ddbb048723004f5f1db1
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-07-08 17:47:37 -0400

    Create multiTable.js

---
+++ multiTable.js
@@ -0,0 +1,5 @@
+//Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+function multiTable(n) {
+  return `1 * ${n} = ${n*1}\n2 * ${n} = ${n*2}\n3 * ${n} = ${n*3}\n4 * ${n} = ${n*4}\n5 * ${n} = ${n*5}\n6 * ${n} = ${n*6}\n7 * ${n} = ${n*7}\n8 * ${n} = ${n*8}\n9 * ${n} = ${n*9}\n10 * ${n} = ${n*10}`  
+}
