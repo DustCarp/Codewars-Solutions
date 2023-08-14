fae5fb8fe51149719253e44bfdda33fcdbd2f912
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-08-13 20:56:35 -0400

    Update rootSum.js

--- rootSum.js
+++ rootSum.js
@@ -1,4 +1,24 @@
+// Find the sum of all positive integer roots up to (and including) the nth term of n, whose products are rational positive integers.
 
+// Simple...
+
+// Right?
+
+// Examples:
+
+// function(1) => 1 as (1root 1 = 1);
+// function(9) => 12 as (1root 9 = 9) + (2root 9 = 3);
+// function(27) => 30 as (1root 27 = 27) + (3root 27 = 3);
+// function(64) => 78 as (1root 64 = 64) + (2root 64 = 8) + (3root 64 = 4) + (6root 64 = 2);
+// Test Cases:
+
+// Five select test cases (which are visible to you).
+// Of the five, the last is the hardest and will test the precision of your floating point values.
+// The first 5000 numbers will be tested.
+// Finally, 100 randomly generated numbers from 1 to 1,000,000 will be tested.
+// Tip:
+
+// Be careful of data types and floating point values
 
 function rootSum(n) {
 
