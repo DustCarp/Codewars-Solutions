b27f9ad8e47ab3ac461b27a32f50cc0efd51e8b0
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-09-07 20:23:02 -0400

    Create moveZeros.js

---
+++ moveZeros.js
@@ -0,0 +1,5 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+var moveZeros = function (arr) {
+  return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+}
