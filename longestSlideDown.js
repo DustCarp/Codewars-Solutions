7914de113413eb6d99222441462124f9b39d1190
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-10-02 20:29:40 -0400

    Update longestSlideDown.js

--- longestSlideDown.js
+++ longestSlideDown.js
@@ -3,7 +3,7 @@
 //    /3/
 //   \7\ 4 
 //  2 \4\ 6 
-// 8 5 \9\ 3
+// 8 5 \9\ 3   
 // Here comes the task...
 // Let's say that the 'slide down' is the maximum sum of consecutive numbers from the top to the bottom of the pyramid. As you can see, the longest 'slide down' is 3 + 7 + 4 + 9 = 23
 
