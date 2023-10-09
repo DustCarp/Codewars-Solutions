9d6c2d2d882c6d1b90520c9511df1f4ba3c2b283
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-10-09 16:46:13 -0400

    Update 3or5Sums.js

--- 3or5Sums.js
+++ 3or5Sums.js
@@ -1,5 +1,7 @@
 //Function that returns the sum of all numbers below a given number which are divisible by 3 or 5. If a number is divisible by both 3 and 5, it is only counted once. 
 
+
+
 function solution(number){
   let num = 0
   for(let i = 0; i<number; i++){
