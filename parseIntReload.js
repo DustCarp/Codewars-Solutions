c7892c7ecbfe3d56160afe60255f81494cdc059b
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-10-08 20:41:43 -0400

    Update parseIntReload.js

--- parseIntReload.js
+++ parseIntReload.js
@@ -1,3 +1,18 @@
+// In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+
+// Examples:
+
+// "one" => 1
+// "twenty" => 20
+// "two hundred forty-six" => 246
+// "seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+// Additional Notes:
+
+// The minimum number is "zero" (inclusively)
+// The maximum number, which must be supported is 1 million (inclusively)
+// The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+// All tested numbers are valid, you don't need to validate them
+
 var words = {
   "zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9, 
   "ten":10, "eleven":11, "twelve":12, "thirteen":13, "fourteen":14, "fifteen":15, "sixteen":16, 
