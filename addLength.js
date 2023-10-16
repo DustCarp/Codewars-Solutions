95c2098ab96723f5f2f87f89e23746771ef3e2b1
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-10-16 16:53:32 -0400

    Update addLength.js

--- addLength.js
+++ addLength.js
@@ -2,9 +2,11 @@
 
 // Note: String will have at least one element; words will always be separated by a space.
 
+
 function addLength(str) {
 let arr = str.split(' ')
 let arr2 = []
+
 for(let i=0; i<arr.length;i++){
   arr2.push(`${arr[i]} ${arr[i].length}`)
 }
