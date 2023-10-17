18890d8f63128f2386fb49150a9e1c45e9583e41
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-10-17 16:54:50 -0400

    Update alternate.js

--- alternate.js
+++ alternate.js
@@ -2,7 +2,9 @@
 
 
 function alternate(n, firstValue, secondValue){
+  
   let arr = []
+
   for(let i=0; i<n; i++){
     if(i%2===0){
       arr.push(firstValue)
@@ -11,4 +13,5 @@ function alternate(n, firstValue, secondValue){
     }
   }
   return arr
+  
 }
