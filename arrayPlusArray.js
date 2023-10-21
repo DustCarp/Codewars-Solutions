d3e125962e81fec2040720b85e0770aac4ed5213
Dustin Carpenter <dustin.carpenter2855@gmail.com>
2023-10-21 17:36:39 -0400

    Update arrayPlusArray.js

--- arrayPlusArray.js
+++ arrayPlusArray.js
@@ -1,5 +1,6 @@
 //Function that takes in 2 arrays and sums the contents of both. 
 
+
 function arrayPlusArray(arr1, arr2) {
   let arr1Sum = arr1.reduce((a,b)=> {return a+b});
   let arr2Sum = arr2.reduce((a,b)=> {return a+b});
